9b7a06689c2fc4df0e7fa7dd440f543f3e0b732f
Aryadip Mridha <mearjo1999@gmail.com>
2023-02-17 00:43:28 +0530

    added client side functions

--- client/script.js
+++ client/script.js
@@ -29,3 +29,50 @@ function typeText(element, text) {
         }
     }, 20)
 }
+
+function generateUniqueId() {
+    const timestamp = Date.now();
+    const randomnumber = Math.random();
+    const hexadecimalString = randomnumber.toString(16);
+
+    return 'id-$(timestamp)-$(hexadecimalString)';
+
+
+}
+
+function chatstripe(isAi, value, uniqueId) {
+    return (
+
+        <div class="wrapper ${isAi && 'ai'}" >
+            <div class="chat" >
+                < div className="profile" >
+                    <img src="${isAi ? bot : user}"
+                        alt="${isAi ? 'bot' : 'user'}" />
+                </div>
+                <div class="message" id="${uniqueId}">${value}</div>
+            </div>
+        </div>
+    )
+}
+
+
+const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const data = newFormData(form);
+
+    //user chatStripe
+    chatContainer.innerHTML += chatstripe(false, data.get('prompt'));
+
+
+    //text area clear
+    FormData.reset();
+
+    //bot's Chatstripe
+    //uid for GPT uid
+    const uniqueId = generateUniqueId();
+    chatContainer.innerHTML += chatstripe(true, " ", uniqueID);
+    // " " because it will be filled later
+
+    chatContainer.scrollTop = chatContainer.scrollHeight;
+}
